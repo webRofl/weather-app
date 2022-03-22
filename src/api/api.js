3f4d57db7c43600739d7d78a476ced618d452635
webRofl <kakaxa1928@mail.ru>
2022-03-22 22:34:35 +0400

    fix network errors

--- src/api/api.js
+++ src/api/api.js
@@ -6,14 +6,14 @@ export const getWeatherInfo = {
   getTownCoordinates: (town) => {
     return axios
       .get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${town}&limit=5&appid=${apiKey}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${town}&limit=5&appid=${apiKey}`
       )
       .then((response) => response.data[0]);
   },
   getTownWeather: (lat, lon) => {
     return axios
       .get(
-        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=0c9bd8408371b8e04459680743159267`
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=0c9bd8408371b8e04459680743159267`
       )
       .then((response) => response.data);
   },
